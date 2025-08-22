39de24bf24c9718a4cf941fed697300b6cc101a7
Husena Indorwala <71762331026@cit.edu.in>
2025-08-22 09:23:29 +0530

    Time: 4 ms (19.99%), Space: 12.5 MB (10.19%) - LeetHub

---
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count=0;
+        for i in range(0,len(nums)):
+            for j in range(0,len(nums)):
+            
+                if i<j and nums[i]==nums[j]:
+                    count=count+1;
+        return count
+        
