51ed04269f315a058b0610c9d409925b8a63a21f
Husena Indorwala <71762331026@cit.edu.in>
2025-08-28 10:43:37 +0530

    Time: 11 ms (59.57%), Space: 14 MB (95.2%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        cnt=Counter(nums)
+        return[num for num,_ in cnt.most_common(k)]
