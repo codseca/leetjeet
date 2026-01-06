894bf14560233a3f48c252e4ecf4a9a14482706e
Husena Indorwala <71762331026@cit.edu.in>
2026-01-06 16:35:44 +0530

    Time: 23 ms (70.11%), Space: 19.4 MB (52.23%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.py
@@ -5,22 +5,35 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        sum1=sum(cardPoints[:k])
+        l,r=k-1,len(cardPoints)-1
+        max_sum=sum1
+        curr_sum=sum1
+        for i in range(0,k):
+            curr_sum=curr_sum-cardPoints[l]
+            curr_sum+=cardPoints[r]
+            l-=1
+            r-=1
+            max_sum=max(max_sum,curr_sum)
 
-        n = len(cardPoints)
+        return max_sum
+
+
+        # n = len(cardPoints)
         
-        if k == n:
-            return sum(cardPoints)
+        # if k == n:
+        #     return sum(cardPoints)
         
-        window_size = n - k
-        total_sum = sum(cardPoints)
+        # window_size = n - k
+        # total_sum = sum(cardPoints)
         
-        window_sum = sum(cardPoints[:window_size])
-        min_window_sum = window_sum
+        # window_sum = sum(cardPoints[:window_size])
+        # min_window_sum = window_sum
         
-        for i in range(window_size, n):
-            window_sum += cardPoints[i]      
-            window_sum -= cardPoints[i - window_size]  
-            min_window_sum = min(min_window_sum, window_sum)
+        # for i in range(window_size, n):
+        #     window_sum += cardPoints[i]      
+        #     window_sum -= cardPoints[i - window_size]  
+        #     min_window_sum = min(min_window_sum, window_sum)
         
-        return total_sum - min_window_sum
+        # return total_sum - min_window_sum
         
