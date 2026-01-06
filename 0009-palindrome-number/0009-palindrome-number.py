78d2be32b04e7642dddd5e697009392b4779ff2d
Husena Indorwala <71762331026@cit.edu.in>
2026-01-06 09:56:39 +0530

    Time: 19 ms (21.25%), Space: 12.4 MB (83.36%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -5,8 +5,24 @@ class Solution(object):
         :rtype: bool
         """
         
-        x=str(x)
-        return x[::]==x[::-1]
+        s=str(x)
+        #return x[::]==x[::-1]
+       
+        # l = 0
+        # r = len(s) - 1
 
+        # while l < r:
+        #     if s[l] != s[r]:
+        #         return False
+        #     l += 1
+        #     r -= 1
 
-            
+        # return True
+
+        org=x
+        rev=0
+        while(x>0):
+            last=x%10
+            rev=(rev*10)+last
+            x=x//10
+        return org == rev
