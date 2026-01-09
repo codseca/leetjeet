a6d24d374d4868192b50db7c9f544a48ca34ff7a
Husena Indorwala <71762331026@cit.edu.in>
2026-01-09 16:27:18 +0530

    Time: 10 ms (48.7%), Space: 13.2 MB (85.76%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        topo = []
+        indeg = [0] * numCourses
+
+        # build graph
+        graph = [[] for _ in range(numCourses)]
+
+        for a, b in prerequisites:
+            graph[b].append(a)   # b -> a
+            indeg[a] += 1        # increase indegree of a
+
+        q = deque()
+        for i in range(numCourses):
+            if indeg[i] == 0:
+                q.append(i)
+
+        while q:
+            node = q.popleft()
+            topo.append(node)
+
+            for j in graph[node]:
+                indeg[j] -= 1
+                if indeg[j] == 0:
+                    q.append(j)
+
+        if len(topo) != numCourses:
+            return False
+
+        return True
