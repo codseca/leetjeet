48c035c4749baef95a914bdb95eb810a1277313e
Husena Indorwala <71762331026@cit.edu.in>
2026-01-29 10:21:32 +0530

    Time: 3 ms (39.37%), Space: 12.7 MB (0%) - LeetHub

--- 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.py
+++ 2455-average-value-of-even-numbers-that-are-divisible-by-three/2455-average-value-of-even-numbers-that-are-divisible-by-three.py
@@ -4,15 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        n=len(nums)
-        c=0
-        a=0
-        for i in range(n):
-            if nums[i]%2==0 and nums[i]%3==0:
-                c+=1
-                a+=nums[i]
+        # n=len(nums)
+        # c=0
+        # a=0
+        # for i in range(n):
+        #     if nums[i]%2==0 and nums[i]%3==0:
+        #         c+=1
+        #         a+=nums[i]
 
             
-        return a//c if c!=0 else 0
+        # return a//c if c!=0 else 0
+
+        a=[x for x in nums if x%6==0]
+        return sum(a)//len(a) if a else 0
 
         
